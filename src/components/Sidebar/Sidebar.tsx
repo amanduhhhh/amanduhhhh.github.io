dc6a74712c54bb093ad0b2999ef7ae59c2409a2c
amanduhhhh <axi@uwaterloo.ca>
2025-01-02 16:53:35 -0500

    tweaking hashrouter oh god

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -18,30 +18,33 @@ function Sidebar() {
         <img src={logo} alt="logo" />
       </Link>
       <nav>
-        <NavLink className={({ isActive }) => (isActive ? "active" : "")} to="">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active" : "")}
+          to="/#/"
+        >
           <FontAwesomeIcon icon={faHome} color="#60837F" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
-          to="about"
+          to="/#/about"
         >
           <FontAwesomeIcon icon={faUser} color="#60837F" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
-          to="projects"
+          to="/#/projects"
         >
           <FontAwesomeIcon icon={faFolder} color="#60837F" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
-          to="design"
+          to="/#/design"
         >
           <FontAwesomeIcon icon={faBrush} color="#60837F" />
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
-          to="contact"
+          to="/#/contact"
         >
           <FontAwesomeIcon icon={faEnvelope} color="#60837F" />
         </NavLink>
