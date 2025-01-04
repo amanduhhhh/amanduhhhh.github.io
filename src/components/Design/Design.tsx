34873129658196c94483f0df63714ef6e840e675
amanduhhhh <axi@uwaterloo.ca>
2025-01-03 21:49:51 -0500

    fixed images

--- src/components/Design/Design.tsx
+++ src/components/Design/Design.tsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 import Loader from "react-loaders";
 import "./Design.scss";
+import img1 from "../../assets/art/celeste4.6CA.png";
+import img2 from "../../assets/art/pipo3.png";
+import img3 from "../../assets/art/landscap1.4.png";
+import img4 from "../../assets/art/uhhhC.png";
+import img5 from "../../assets/art/500days.png";
 
 const Design = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
   const [imageClass, setImageClass] = useState("image-animate");
-  const imgs = [
-    "src/assets/art/celeste4.6CA.png",
-    "src/assets/art/pipo3.png",
-    "src/assets/art/landscap1.4.png",
-    "src/assets/art/uhhhC.png",
-    "src/assets/art/500days.png",
-  ];
+  const imgs = [img1, img2, img3, img4, img5];
   useEffect(() => {
     const timer = setTimeout(() => {
       setImageClass("image-hover");
