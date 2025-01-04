d79a80d30e8b3d28b65767188ee64fe01e7dae18
amanduhhhh <axi@uwaterloo.ca>
2025-01-03 21:02:41 -0500

    added design page and added a little more responsiveness.. kinda

--- src/components/Design/Design.tsx
+++ src/components/Design/Design.tsx
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 import Loader from "react-loaders";
+import "./Design.scss";
 
 const Design = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
+  const [imageClass, setImageClass] = useState("image-animate");
+  const imgs = [
+    "src/assets/art/celeste4.6CA.png",
+    "src/assets/art/pipo3.png",
+    "src/assets/art/landscap1.4.png",
+    "src/assets/art/uhhhC.png",
+    "src/assets/art/500days.png",
+  ];
   useEffect(() => {
-    const timer = setTimeout(() => setLetterClass("text-animate-hover"), 2600);
+    const timer = setTimeout(() => {
+      setImageClass("image-hover");
+      setLetterClass("text-animate-hover");
+    }, 3600);
     return () => clearTimeout(timer);
   }, []);
   return (
@@ -19,7 +31,19 @@ const Design = () => {
               idx={10}
             />
           </h1>
-          <p>This page will be up shortly!!</p>
+          <p>
+            <span className="highlighted-2"> Programs: </span>Clip Studio Paint,
+            Adobe Photoshop
+          </p>
+        </div>
+        <div className="gallery">
+          {imgs.map((pic, index) => {
+            return (
+              <div className="img-container">
+                <img className={imageClass} src={pic} id={"img_" + index} />
+              </div>
+            );
+          })}
         </div>
       </div>
       <Loader type="pacman" active={true} />
