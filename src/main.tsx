5b55f28673b9c2e0e3bbe11c48b9608ff44378d1
amanduhhhh <amandaxi2024@gmail.com>
2026-03-07 15:41:53 -0500

    package updates

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.scss";
 import App from "./App.tsx";
 import { HashRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HashRouter>
   </StrictMode>,
 );
