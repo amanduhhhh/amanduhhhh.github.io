9990e9c92c1017dedf1af56d44f1d85f32a7101c
amanduhhhh <amandaxi2024@gmail.com>
2026-03-20 22:26:46 -0400

    update about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -33,43 +33,57 @@ const About = () => {
           </h1>
           <p>
             I'm a <span className="highlighted-2">CS + Co-op student</span> at
-            the University of Waterloo. I've built countless full-stack applications, from web apps for crochet pattern design to patient portal mobile apps. Learn more in my {" "}
+            the University of Waterloo. I've built countless full-stack
+            applications, from web apps for crochet pattern design to patient
+            portal mobile apps. Learn more in my{" "}
             <Link to="/projects">projects!</Link>
           </p>
           <p>
-            Or, for just the gist of it, take a look at my {" "}
+            Or, for just the gist of it, take a look at my{" "}
             <a target="blank" href={resumeLink}>
               resume.
             </a>
           </p>
           <p>
-            Besides coding, I'm an absolute puzzle fiend. Be it <span className="highlighted-2">minesweeper</span>, <span className="highlighted-2">crosswords</span>, or <span className="highlighted-2">90's point and click adventure games</span> (LucasArts especially), if I'm not coding, I've got my head buried in one of these.
+            Besides coding, I'm an absolute puzzle fiend. Be it{" "}
+            <span className="highlighted-2">minesweeper</span>,{" "}
+            <span className="highlighted-2">crosswords</span>, or{" "}
+            <span className="highlighted-2">
+              90's point and click adventure games
+            </span>{" "}
+            (LucasArts especially), if I'm not coding, I've got my head buried
+            in one of these.
           </p>
           <p>
-            That, or a drawing. {" "}
-            <Link to="/design">Take a peek at my art!</Link>
+            That, or a drawing. <Link to="/design">Take a peek at my art!</Link>
           </p>
           <h2>what i've been up to:</h2>
           <ul>
             <li>
-              Currently in the process of completing a {" "}
+              Currently a platform developer for{" "}
+              <span className="highlighted-2">Hack The North!</span>
+            </li>
+            <li>
+              In the process of completing a{" "}
+              <span className="highlighted-2">Ubisoft mentorship</span>, where
+              I'll be developing a game under industry leadership!
+            </li>
+            <li>
+              I also served as Waterloo's{" "}
               <span className="highlighted-2">
-                Ubisoft mentorship
-              </span>, where I'll be developing a game under industry leadership!
+                Campus Crusade for Cheese president
+              </span>
+              . I have thenceforth committed myself to sampling one new cheese
+              every week. Saint Albray's is a recent favourite.
             </li>
             <li>
-              This term, I'll also be a developer on <span className="highlighted-2">UWaterloo's Blueprint chapter</span>.
-            </li>
-            <li>
-              I also served as Waterloo's <span className="highlighted-2">Campus Crusade for Cheese president</span>. 
-              I have thenceforth committed myself to sampling one new cheese every week. 
-              Saint Albray's is a recent favourite.
-            </li>
-            <li>
-              From May - December 2025, I completed a software internship at {""}
-              <span className="highlighted-2">
-                Oak Ridges Heart Centre
-              </span>, where I developed an <span className="highlighted-2">AI clinic scribe</span>, a <span className="highlighted-2">food portion size model</span>, and a <span className="highlighted-2">mobile app</span>, amongst other things.  
+              From May - December 2025, I completed a software internship at{" "}
+              <span className="highlighted-2">Oak Ridges Heart Centre</span>,
+              where I developed an{" "}
+              <span className="highlighted-2">AI clinic scribe</span>, a{" "}
+              <span className="highlighted-2">food portion size model</span>,
+              and a <span className="highlighted-2">mobile app</span>, amongst
+              other things.
             </li>
           </ul>
         </div>
